adb4867c49e7787f2725c77c24f3c17fce63358c
Benjamin Shyong <ben.shyong@gmail.com>
2014-01-28 21:13:44 -0500

    initial commit

---
+++ digit-recognition/knn_python.py
@@ -0,0 +1,8 @@
+# An experiment in handwritten digit classification using KNN.
+# (c) 2014. Benjamin Shyong <hello@benshyong.com>.
+# All rights reserved.
+
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+
